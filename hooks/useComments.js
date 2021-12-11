1757ebf08335e7cedddd6a391655c931096529e1
Muhammed Yusuf <myusuf2121@gmail.com>
2021-12-11 15:47:09 +0300

    Switched to axois in all project.

--- hooks/useComments.js
+++ hooks/useComments.js
@@ -1,4 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 export default function useComments(params) {
@@ -11,10 +12,9 @@ export default function useComments(params) {
   const fetchComment = async () => {
     const query = new URLSearchParams({ url })
     const newUrl = `/api/comment?${query.toString()}`
-    const response = await fetch(newUrl, {
-      method: 'GET'
-    })
-    const data = await response.json()
+    const response = await axios.get(newUrl)
+    const data = await response.data
+
     commentsSet(data)
   }
 
@@ -31,12 +31,21 @@ export default function useComments(params) {
   const onSubmit = async e => {
     e.preventDefault()
     const userToken = await getAccessTokenSilently()
+    // const opts = {
+    //   url: '/api/comment',
+    //   data: { text, userToken, url }
+    // }
+  
+    // console.log(opts);
 
-    await fetch('/api/comment', {
-      method: 'POST',
-      body: JSON.stringify({ text, userToken, url }),
-      headers: { 'Content-Type': 'application/json' }
-    })
+    // await fetch('/api/comment', {
+    //   method: 'POST',
+    //   body: JSON.stringify({ text, userToken, url }),
+    //   headers: { 'Content-Type': 'application/json' }
+    // })
+
+    await axios.post('/api/comment',{ text, userToken, url })
+
 
     fetchComment()
     textSet('')
--- pages/api/comment.js
+++ pages/api/comment.js
@@ -1,57 +1,63 @@
-import { nanoid } from "nanoid";
-import Redis from "ioredis";
-import { Boom } from "@hapi/boom";
-
+import { nanoid } from 'nanoid'
+import Redis from 'ioredis'
+import { Boom } from '@hapi/boom'
+import axios from 'axios'
 function errorResponse(res, error) {
-  const { output } = error;
-  return res.status(output.statusCode).json(output.payload);
+  const { output } = error
+  return res.status(output.statusCode).json(output.payload)
 }
 
 export default async function handler(req, res) {
   //create
-  if (req.method === "POST") {
-    const { url, userToken, text } = req.body;
+  if (req.method === 'POST') {
+    const { url, userToken, text } = req.body
     if (!url || !userToken || !text) {
       errorResponse(
         res,
-        Boom.badData("your data is bad you should feel bad :))")
-      );
+        Boom.badData('your data is bad you should feel bad :))')
+      )
     }
-    const userResponse = await fetch(
-      `https://${process.env.NEXT_PUBLIC_AUTH0_DOMAIN}/userinfo`,
-      {
-        headers: {
-          Authorization: `Bearer ${userToken}`,
-          "Content-Type": "application/json",
-        },
+    const userResponse = await axios.get(`https://${process.env.NEXT_PUBLIC_AUTH0_DOMAIN}/userinfo`,{
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        'Content-Type': 'application/json'
       }
-    );
-
-    const user = await userResponse.json();
+    })
+    // const userResponse = await fetch(
+    //   `https://${process.env.NEXT_PUBLIC_AUTH0_DOMAIN}/userinfo`,
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${userToken}`,
+    //       'Content-Type': 'application/json'
+    //     }
+    //   }
+    // )
+    
+    const user = await userResponse.data
     const comment = {
       id: nanoid(),
       createdAt: Date.now(),
       text,
       user: {
         name: user.name,
-        picture: user.picture,
-      },
-    };
+        picture: user.picture
+      }
+    }
 
-    let redis = new Redis(process.env.REDIS_URL);
-    redis.lpush(url, JSON.stringify(comment));
-    redis.quit();
+    let redis = new Redis(process.env.REDIS_URL)
+    redis.lpush(url, JSON.stringify(comment))
+    redis.quit()
 
-    res.status(200).json(comment);
+    res.status(200).json(comment)
   }
   //fetch
-  if (req.method === "GET") {
-    const { url } = req.query;
-    let redis = new Redis(process.env.REDIS_URL);
-    const comments = await redis.lrange(url, 0, -1);
-    redis.quit();
+  if (req.method === 'GET') {
+    const { url } = req.query
+    let redis = new Redis(process.env.REDIS_URL)
+    const comments = await redis.lrange(url, 0, -1)
+    redis.quit()
 
-    const data = comments.map((o) => JSON.parse(o));
-    res.status(200).json(data);
+    const data = comments.map(o => JSON.parse(o))
+    res.status(200).json(data)
   }
 }
