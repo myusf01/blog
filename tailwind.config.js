b2a6a2d62666c7da264059ce755d84458b9f5c9f
Muhammed Yusuf <myusuf2121@gmail.com>
2022-01-15 00:54:29 +0300

    Update font family.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      readex: '"Readex Pro"',
-      jetbrain: '"JetBrains Mono"'
+      sans: ['"Readex Pro"', ...fontFamily.sans],
+      mono: ['"JetBrains Mono"', ...fontFamily.mono],
     },
     // extend: {
     //   typography: {
@@ -25,13 +27,16 @@ module.exports = {
           css: {
 
             h1: {
-              color: theme('colors.gray.800')
+              color: theme('colors.gray.700')
             },
             h2: {
-              color: theme('colors.gray.800')
+              color: theme('colors.gray.700')
             },
             h3: {
-              color: theme('colors.gray.800')
+              color: theme('colors.gray.700')
+            },
+            h4: {
+              color: theme('colors.gray.700')
             },
 
           },
