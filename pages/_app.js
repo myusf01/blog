675d3830542b720aa3b2e421941fc5f4ad0eabc1
Muhammed Yusuf <myusuf2121@gmail.com>
2022-01-23 22:24:47 +0300

    test.

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,7 @@ function MyApp({ Component, pageProps }) {
             <title>muhammed yusuf</title>
             <meta property="og:title" content="muhammed yusuf - blog" />
             <meta property="og:url" content={`https://myusuf.net/`} />
-            <meta property="og:image" content='https://myusuf.net/_next/image?url=/public/img/ben.jpg' />
+            <meta property="og:image" content='https://myusuf.net/_next/image?url=/public/img/ben.jpg&w=3840&q=75' />
             <meta property="og:type" content="website" />
             <meta property="og:description" content="merhaba, bloguma hoş geldin." />
             <meta name="twitter:card" content="summary_large_image"></meta>
